212483692456740551a47a2a2bdbdfa0d588c91c
sam <baskicisamet@gmail.com>
2018-02-10 13:55:19 +0300

    adding bootstrap again

--- src/main/java/com/sam/spring5mvcrestapp/controller/v1/CategoryController.java
+++ src/main/java/com/sam/spring5mvcrestapp/controller/v1/CategoryController.java
@@ -24,7 +24,12 @@ public class CategoryController {
 	@GetMapping
 	public ResponseEntity<CategoryListDTO> getAllCategories(){
 		
+		System.out.println("DEBUG INFO :::::::::::::::::::::::: request catched");
+		System.out.println("DEBUG INFO :::::::::::::::::::::::: categori list size : " + categoryService.getAllCategories().size());
+		System.out.println("DEBUG INFO :::::::::::::::::::::::: categori size inside listDTO : " + new CategoryListDTO(categoryService.getAllCategories()).getCategories().size() );
+		
 		return new ResponseEntity<CategoryListDTO>(new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+		
 	}
 	
 	
