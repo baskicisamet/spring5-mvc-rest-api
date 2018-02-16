68fa74edba757742e3085c395203d046a7fc174e
sam <baskicisamet@gmail.com>
2018-02-16 04:29:53 +0300

    splitting into modules

--- src/main/java/com/sam/spring5mvcrestapp/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sam.spring5mvcrestapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sam.spring5mvcrestapp.domain.Customer;
-
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-}
--- src/main/java/com/sam/spring5mvcrestapp/repository/VendorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sam.spring5mvcrestapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sam.spring5mvcrestapp.domain.Vendor;
-
-
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-}
